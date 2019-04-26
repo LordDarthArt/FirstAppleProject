789bb7aa67cdb793382a4dd8004e4b9fe68ef724
Artyom Tarasov <tarasov@ittest-team.ru>
2019-04-27 02:15:55 +0300

    Initial Commit

---
+++ FirstAppleProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FirstAppleProject
+//
+//  Created by Test on 27/04/2019.
+//  Copyright © 2019 LordDarthArt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirstAppleProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FirstAppleProject
+//
+//  Created by Test on 27/04/2019.
+//  Copyright © 2019 LordDarthArt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
