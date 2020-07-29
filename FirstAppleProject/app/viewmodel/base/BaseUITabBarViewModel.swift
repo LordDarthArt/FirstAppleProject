2b61a261ad35c43064891a74185f2f179e9fe931
LordDarthArt <lorddarthart@gmail.com>
2020-07-29 22:22:52 +0300

    Sync commit

---
+++ FirstAppleProject/app/viewmodel/base/BaseUITabBarViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  BaseUITabBarViewController.swift
+//  FirstAppleProject
+//
+//  Created by Тарасов Артём Максимович on 13.07.2020.
+//  Copyright © 2020 LordDarthArt. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+struct BaseUITabBarViewModel {
+    
+}
